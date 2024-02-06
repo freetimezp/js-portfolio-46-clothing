a85474ebc0eea9ed555d2c0563f3ef5d215bf92c
freetimezp <kraftukrnet@ukr.net>
2024-02-06 17:48:46 +0200

    06. js-portfolio-46-clothing. Animation use ScrollReveal.

--- assets/js/main.js
+++ assets/js/main.js
@@ -49,7 +49,30 @@ let swiperHome = new Swiper('.home__swiper', {
 });
 
 
+//change bg on scroll
+const bgHeader = () => {
+    const header = document.getElementById('header');
+    this.scrollY >= 10
+        ? header.classList.add("bg-header")
+        : header.classList.remove("bg-header");
+}
 
+window.addEventListener("scroll", bgHeader);
+
+
+//animation on scroll
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '100px',
+    duration: 2500,
+    reset: true
+});
+
+sr.reveal(`.home__swiper, .home__footer`, { scale: .7 });
+sr.reveal(`.home__circle`, { scale: 1.5, delay: 300 });
+sr.reveal(`.home__subcircle`, { scale: 1.8, delay: 600 });
+sr.reveal(`.home__title`, { scale: 3, origin: 'bottom', delay: 1200 });
+sr.reveal(`.swiper-button-prev, .swiper-button-next`, { origin: 'bottom' });
 
 
 
